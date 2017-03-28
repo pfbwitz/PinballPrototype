4beadd3c83d58613944992dd89cb9f6f4a07d096
hsmadi <housamsmadi@gmail.com>
2017-03-28 02:04:34 +0200

    Add button for each game to run the tracker and the game directly

--- DepthTracker/UI/TrackerPicker.xaml.cs
+++ DepthTracker/UI/TrackerPicker.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DepthTracker.UI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,16 +30,32 @@ namespace DepthTracker.UI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var content = ((Button)sender).Content.ToString();
+            Process Pinballgame = new Process();
+            Pinballgame.StartInfo.FileName = @"C:\CLoudabit\Pinball.lnk";
+            Process CarGame = new Process();
+            CarGame.StartInfo.FileName = @"C:\CLoudabit\Racing.lnk";
+            Process KingsGame = new Process();
+            KingsGame.StartInfo.FileName = @"C:\CLoudabit\Kings.lnk";
+            Process NeverGame = new Process();
+            NeverGame.StartInfo.FileName = @"C:\CLoudabit\NeverHaveIEver.lnk";
+
             switch (content)
             {
+
                 case "Pinball":
+                    Pinballgame.Start();
                     new PinballTracker().ShowDialog();
-                    
                     break;
                 case "Car":
+                    CarGame.Start();
                     new CarTracker().ShowDialog();
                     break;
-                case "Clicks":
+                case "Kings":
+                    KingsGame.Start();
+                    new ClicksTracker().ShowDialog();
+                    break;
+                case "Never":
+                    NeverGame.Start();
                     new ClicksTracker().ShowDialog();
                     break;
             }
